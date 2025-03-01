dd99b17488e3cfdc5a0df52c75b32df34ad0ca32
Serey Roth <serey.roth@alldrafts.com>
2025-02-28 20:37:40 -0800

    Add status bar.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack>
-    {/* <Stack.Screen name="index" options={{
-      title: 'Home',
-    }} />
-    <Stack.Screen name="about" options={{
-      title: 'About'
-    }} /> */}
-    <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-    <Stack.Screen name='+not-found' />
-  </Stack>
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='+not-found' />
+      </Stack>
+      <StatusBar style='light' />
+    </>
+  )
 }
